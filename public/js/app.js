25f7ad03c4219078ea8ac9941b6b5ddcfd40267b
alex <alexanderlopez@outlook.com>
2016-01-21 00:48:47 -0600

    modularizando

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,26 @@
 
   (function () {
   var app = angular.module('myApp', [
+    'ngRoute',
     'ngMaterial',
     'ngMessages',
     'colors.controller'
   ]);
 
+  app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+      .when('/cartillas', {
+        templateUrl: 'views/cartillas.html'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'DemoCtrl'
 
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
 
 
 })();
